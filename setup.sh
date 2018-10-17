ff7365e04233bc686fa3f75fff28837193af297e
Thom Carter <thom@tjvc.uk>
2018-10-17 12:11:53 +0100

    Add rubocop.yml

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/profile ~/.profile
 ln -sf ~/dotfiles/pryrc ~/.pryrc
+ln -sf ~/dotfiles/rubocop.yml ~/.rubocop.yml
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/vimrc ~/.vimrc
 
