a2c02b6b0bd3d390ec22aa9e08c087480b0703e1
Thom Carter <thom@tjvc.uk>
2019-09-10 12:48:28 +0100

    Add Neovim configuration

--- setup.sh
+++ setup.sh
@@ -16,6 +16,7 @@ ln -sf ~/dotfiles/vimrc ~/.vimrc
 
 mkdir -p ~/.config
 [ ! -d ~/.config/base16-shell ] && ln -s ~/dotfiles/config/base16-shell ~/.config/base16-shell
+[ ! -d ~/.config/nvim ] && ln -s ~/dotfiles/config/nvim ~/.config/nvim
 
 source ~/.profile
 
