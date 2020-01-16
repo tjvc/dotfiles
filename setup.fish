6278e123d555bdb7701df644176353abfca38e33
Thom Carter <thom@tjvc.uk>
2020-01-16 19:34:31 +0000

    Set fish abbreviations

--- setup.fish
+++ setup.fish
@@ -1,3 +1,4 @@
 set fish_greeting
-set -U fish_user_paths /usr/local/opt/coreutils/libexec/gnubin
+set -U fish_user_paths /usr/local/opt/coreutils/libexec/gnubin /usr/local/opt/python/libexec/bin
 set -U EDITOR vim
+fish abbr.fish
