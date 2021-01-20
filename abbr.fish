31d87a40ddf1ec7b9f37c3759c1f9e8c5a418215
Thom Carter <thom@tjvc.uk>
2021-01-20 16:07:36 +0000

    Add more abbreviations

--- abbr.fish
+++ abbr.fish
@@ -1,4 +1,6 @@
+abbr -a -U -- b bundle
 abbr -a -U -- be 'bundle exec'
+abbr -a -U -- dc docker-compose
 abbr -a -U -- ga 'git add'
 abbr -a -U -- gap 'git add -p'
 abbr -a -U -- gb 'git branch'
@@ -14,6 +16,7 @@ abbr -a -U -- gp 'git push -u'
 abbr -a -U -- gpf 'git push --force-with-lease'
 abbr -a -U -- grbc 'git rebase --continue'
 abbr -a -U -- gst 'git status'
+abbr -a -U -- ll 'ls -alFh'
 abbr -a -U -- sc 'bin/rails console'
 abbr -a -U -- ss 'bin/rails server'
 abbr -a -U -- y yarn
