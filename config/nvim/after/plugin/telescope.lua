6c2dab58073e5e74d815f4b37a813607cff5b94b
Thom Carter <thom@tjvc.uk>
2023-12-29 08:56:27 +0000

    Add space before grep_string input

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -2,7 +2,7 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
 vim.keymap.set('n', '<leader>w', builtin.grep_string, {})
-vim.keymap.set('n', '<leader>a', function() builtin.grep_string({ search = vim.fn.input('>') }) end)
+vim.keymap.set('n', '<leader>a', function() builtin.grep_string({ search = vim.fn.input('> ') }) end)
 vim.keymap.set('n', '<leader>g', builtin.tags, {})
 vim.keymap.set('n', '<leader>h', builtin.oldfiles, {})
 
